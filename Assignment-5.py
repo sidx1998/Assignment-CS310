c8d459957d8b5aac4b5818b067169a290e43ba2d
ZeHaR <anas14529@gmail.com>
2019-12-30 20:45:00 +0500

    Assignments

---
+++ Assignment-5.py
@@ -0,0 +1,24 @@
+#Conversing with python
+
+print("Hello World")
+
+print("You must be the legendary god that comes from the sky")
+
+print("We have been waiting for you for a long time")
+
+print('Our legend says you will be very tasty with mustard')
+
+x=43
+x = x+1
+print(x)
+
+
+# Errors which May Occur
+
+primt ("Hello World")
+
+print "Hello World"
+
+I hate you python
+
+I come in peace, please take me to your leader
---
+++ Assignment-6.py
@@ -0,0 +1,58 @@
+#Program 1
+name = 'Name: '
+inp = input(name)
+print('Hello ' + inp)
+
+Program 2
+hours = "Enter Hours: "
+inp_hrs = float(input(hours))
+rate = "Enter Rate Per Hour: "
+inp_rate = float(input(rate))
+gross = inp_hrs*inp_rate
+print("Pay = ",gross)
+
+#Program 3
+width = 17
+height = 12.0
+
+exp_1 = width//2;
+
+print("Width//2 = ",exp_1)
+print("Type = ",type(exp_1))
+
+exp_2 = width/2.0;
+
+print("Width/2.0 = ",exp_2)
+print("Type = ",type(exp_2))
+
+exp_3 = height/3;
+
+print("height/3 = ",exp_3)
+print("Type = ",type(exp_3))
+
+exp_4 = 1+2*5;
+
+print("1 + 2 * 5 = ",exp_4)
+print("Type = ",type(exp_4))
+
+
+#Program 4
+
+inp_c = float(input("Enter Temperature in Celcius: "))
+farnh = (inp_c*9/5)+32
+print("Temperature in Farenheite: ",farnh,"F")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
